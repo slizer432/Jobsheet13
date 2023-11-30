b71e7244ba33c45c3219df5b7528a420f2a5a91c
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-11-30 14:58:06 +0700

    update tugas 3

--- Tugas3.java
+++ Tugas3.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
 
 public class Tugas3 {
+    static boolean prima;
     public static void main(String[] args) {
         Scanner input01 = new Scanner(System.in);
         System.out.print("Masukkan nilai  : ");
         int n = input01.nextInt();
-
-        if (cetakPrimaRekursif(n, 2)){
+        cekPrimaRekursif(n, (n-1));
+        
+        if (prima == true){
             System.out.println(n + " adalah bilangan prima");
         } else {
             System.out.println(n + " bukan bilangan prima");
         }
         input01.close();
     }
-    public static boolean cetakPrimaRekursif(int n, int pembagi){
-        if (n <= 1){
-            return false;
-        } else if (n % pembagi == 0) {
-            return false;
-        } else {
-            return cetakPrimaRekursif(n, pembagi + 1);
-        }
+    public static boolean cekPrimaRekursif(int n, int pembagi){
+        if (n % pembagi != 0) {
+            return cekPrimaRekursif(n, pembagi - 1);
+        }else if (pembagi != 1 && n % pembagi == 0){
+            return prima = false;
+        }else if (pembagi == 1 && n % pembagi == 0){
+            return prima = true;
+        }else 
+        return prima = false;
     }
 }
